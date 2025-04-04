0ed6581b2323d545b98b8b43105df8944c5031c9
Denip-ctrl <denip2016@gmail.com>
2025-04-04 17:13:35 +0700

    tanpa node-fetch

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -1,12 +1,11 @@
 exports.handler = async () => {
-    const fetch = require('node-fetch');
     const apiKey = process.env.GOOGLE_SHEETS_API_KEY; // Secure API key from Netlify env
     const spreadsheetId = "19thbKkn07eHqgebQzS4gVo4i_J-HSO9Xz8TNd76zcqU";
     const range = "Dialog1!A2:H10";
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
 
     try {
-        const response = await fetch(url);
+        const response = await fetch(url); // No need for require('node-fetch')
         const data = await response.json();
         return { statusCode: 200, body: JSON.stringify(data) };
     } catch (error) {
